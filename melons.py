622b63770025d658d6f7fa641112a6b14df375a5
Hackbright Student <info@hackbrightacademy.com>
2015-04-15 13:11:43 -0700

    we created all the classes for melons

--- melons.py
+++ melons.py
@@ -1,5 +1,10 @@
 """This file should have our melon-type classes in it."""
-
+# default price = $5
+# casabas and ogen += $1
+# if imported = 1.5x more
+# if square = 2x more
+# Deal: 3 or more Wmelons = 0.75x
+# Deal: 5 or more Cant = 0.5x
 
 class WatermelonOrder(object):
     species = "Watermelon"
@@ -10,7 +15,143 @@ class WatermelonOrder(object):
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
-
-        total = 0   # TODO, calculate the real amount!
+        # TODO, calculate the real amount!
+        total = qty * 5.00
+        if qty >= 3:
+            total *= 0.75
 
         return total
+
+        
+class CantaloupeOrder(object):
+    species = "Cantaloupe"
+    color = "tan"
+    imported = False
+    shape = 'natural'
+    seasons = ['Spring', 'Summer']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+        # TODO, calculate the real amount!
+        total = qty * 5.00
+        if qty >= 5:
+            total *= 0.5
+
+        return total
+
+class CasabaOrder(object):
+    species = "Casaba"
+    color = "green"
+    imported = True
+    shape = 'natural'
+    seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+        # TODO, calculate the real amount!
+        total = (qty * (5.00 + 1.00)) * 1.50
+       
+        return total
+
+class SharlynOrder(object):
+    species = "Sharlyn"
+    color = "tan"
+    imported = True
+    shape = 'natural'
+    seasons = ['Summer']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+        # TODO, calculate the real amount!
+        total = (qty * 5.00) * 1.50
+       
+        return total
+
+class SantaClausOrder(object):
+    species = "Santa Claus"
+    color = "green"
+    imported = True
+    shape = 'natural'
+    seasons = ['Winter', 'Spring']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+        # TODO, calculate the real amount!
+        total = (qty * 5.00) * 1.50
+       
+        return total
+
+class ChristmasOrder(object):
+    species = "Christmas"
+    color = "green"
+    imported = False
+    shape = 'natural'
+    seasons = ['Winter', 'Spring']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+        # TODO, calculate the real amount!
+        total = qty * 5.00
+       
+        return total
+
+class HornedMelonOrder(object):
+    species = "Horned Melon"
+    color = "yellow"
+    imported = True
+    shape = 'natural'
+    seasons = ['Summer']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+        # TODO, calculate the real amount!
+        total = qty * 5.00
+        
+        if self.imported == True:
+            total *= 1.5
+        
+        return total
+
+class XiguaOrder(object):
+    species = "Xigua"
+    color = "black"
+    imported = True
+    shape = 'square'
+    seasons = ['Summer']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+        # TODO, calculate the real amount!
+        
+        total = qty * 5.00
+  
+        if self.imported == True:
+            total *= 1.50
+
+        if self.shape == "square":
+            total *= 2.00
+        
+        return total
+
+class OgenOrder(object):
+    species = "Ogen"
+    color = "tan"
+    imported = False
+    shape = 'natural'
+    seasons = ['Spring', 'Summer']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+        # TODO, calculate the real amount!
+        total = qty * 5.00
+        
+        if self.imported == True:
+            total *= 1.50
+
+        if self.shape == "square":
+            total *= 2.00
+        
+        return total
+
+        # if self.name == "Casaba" or self.name == "Ogen":
+        # qty += 1.00
